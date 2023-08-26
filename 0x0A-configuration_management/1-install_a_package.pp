6cf671cd1eb63939f167480b3016a1a1448b1e83
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-26 04:40:46 +0300

    1. Install a package fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # Install a package
 node default {
-    package {'python3-flask':
-        ensure => '2.1.0',
+    package {'flask':
+        ensure   => '2.1.0',
+        provider => 'pip3',
     }
 }
