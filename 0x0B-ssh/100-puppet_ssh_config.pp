0e1ef5c7f00758075c4bf9facbd221bdf64a2775
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-08-26 20:12:24 +0300

    4. Client configuration file (w/ Puppet) fix

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,17 @@
 # Client configuration file (w/ Puppet)
+
+include stdlib
+
 node default {
     file_line { 'Identity':
         ensure  => 'present',
-        line    => 'IdentityFile ~/.ssh/school'
+        line    => 'IdentityFile ~/.ssh/school',
         path    => '~/.ssh/config'
     }
 
     file_line { 'password_off':
         ensure  => 'present',
-        line    => 'PasswordAuthentication no'
+        line    => 'PasswordAuthentication no',
         path    => '~/.ssh/config'
     }
 
