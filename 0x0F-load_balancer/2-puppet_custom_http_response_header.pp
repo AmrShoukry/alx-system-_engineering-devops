317627c29ff1a8ae934125f9e469c63e99eb7fb2
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-04 14:23:48 +0300

    2-puppet_custom_http_response_header.pp fix

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,6 +1,50 @@
 # custom http header
-file_line { 'new_header'
-    path  => '/etc/nginx/sites-available/default',
-    line  => 'add_header X-Served-By $HOSTNAME';
-    after => '    listen [::]:80 default_server;'
+
+$content = "
+server {
+    error_page 404 /custom404.html;
+    location = /custom404.html {
+        root /usr/share/nginx/html;
+        internal;
+    }
+
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+    add_header X-Served-By ${::hostname};
+
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+
+	server_name _;
+
+    location /redirect_me {
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+
+	location / {
+		try_files $uri $uri/ =404;
+	}
+}
+"
+
+exec { 'apt-get update -y':
+    command => 'apt-get update -y',
+    path    => '/usr/bin:/usr/sbin',
+}
+
+package { 'nginx':
+    ensure  => 'installed',
+    require => Exec['apt-get update -y'],
+}
+
+file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => $content,
+    require => Package['nginx'],
+}
+
+service { 'nginx':
+    ensure  => 'running',
+    require => File['/etc/nginx/sites-available/default']
 }
