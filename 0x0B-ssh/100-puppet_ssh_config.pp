21ca07e00ce084ef6da5a1b99a006d1fe100d663
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-04 12:31:31 +0300

    [200~0. Double the number of webservers

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,9 +0,0 @@
-# Client configuration file (w/ Puppet)
-
-node default {
-    file { '/etc/ssh/ssh_config':
-        ensure => 'file',
-        mode   => '0600',
-        content => "IdentityFile ~/.ssh/school\nPasswordAuthentication no\n",
-    }
-}
