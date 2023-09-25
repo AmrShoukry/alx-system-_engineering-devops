0dd92dc35c7f4a6ff03cb88a9c54b263714b4e9a
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-25 14:55:23 +0300

    1. Export to CSV

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" 1. Export to CSV """
+
+import csv
+import json
+import requests
+import sys
+
+if __name__ == '__main__':
+    USER_ID = int(sys.argv[1])
+
+    todo_response = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+{USER_ID}")
+
+    completed_titles = []
+
+    objects = json.loads(todo_response.text)
+    user = json.loads(user_response.text)
+
+    USER_NAME = user.get('name')
+    USER_ID_STRING = sys.argv[1]
+
+    for object in objects:
+        if object.get('userId') == USER_ID:
+            if object.get('completed') is True:
+                new_entry = []
+                new_entry.append(USER_ID)
+                new_entry.append(USER_NAME)
+                new_entry.append(object.get('completed'))
+                new_entry.append(object.get('title'))
+                completed_titles.append(new_entry)
+
+    csv_file_name = f"{USER_ID}.csv"
+
+    with open(csv_file_name, mode='w', newline='') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+        writer.writerows(completed_titles)
