c7ad18772430fd708030d95e4ed790855ae2b696
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-09-25 14:38:10 +0300

    0. Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
 """ 0. Gather data from an API """
 
+import json
 import requests
 import sys
-import json
 
-USER_ID = int(sys.argv[1])
+if __name__ == '__main__':
+    USER_ID = int(sys.argv[1])
 
-todos_response = requests.get("https://jsonplaceholder.typicode.com/todos/")
-user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/\
-{USER_ID}")
+    todo_response = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/\
+    {USER_ID}")
 
-completed = 0
-all = 0
-completed_titles = []
+    complete = 0
+    all = 0
+    completed_titles = []
 
-objects = json.loads(todos_response.text)
-user = json.loads(user_response.text)
+    objects = json.loads(todo_response.text)
+    user = json.loads(user_response.text)
 
-for object in objects:
-    if object['userId'] == USER_ID:
-        all += 1
-        if object['completed'] is True:
-            completed += 1
-            completed_titles.append(object['title'])
+    for object in objects:
+        if object.get('userId') == USER_ID:
+            all += 1
+            if object.get('completed') is True:
+                complete += 1
+                completed_titles.append(object['title'])
 
-print(f"Employee {user['name']} is done with tasks({completed}/{all}):")
+    print(f"Employee {user.get('name')} is done with tasks({complete}/{all}):")
 
-for title in completed_titles:
-    print(f"\t {title}")
+    for title in completed_titles:
+        print(f"\t {title}")
