df06bac7045f2eb72b8229b7bbca1d27fd3dfb22
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-10-10 15:35:09 +0300

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 0. How many subs? """
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ returns the number of subscribers for a given subreddit """
+    headers = {"User-Agent": "AmrShoukry"}
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    data = response.json()
+
+    if response.status_code == 200:
+        return (data['data']['subscribers'])
+    return 0
