72a27f37226097a4c3a5f2a7f4b2e366c231b281
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-10-19 14:59:42 +0300

    user limits

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,11 +2,11 @@
 
 exec {'change-os-configuration-for-holberton-user':
     command => 'sed -i "s/4/4096/" /etc/security/limits.conf',
-    path => '/bin'
+    path    => '/bin'
 }
 
 exec {'change-os-configuration-for-holberton-user2':
-    command => 'sed -i "s/5/4096/" /etc/security/limits.conf',
-    path => '/bin',
+    command   => 'sed -i "s/5/4096/" /etc/security/limits.conf',
+    path      => '/bin',
     subscribe => Exec['change-os-configuration-for-holberton-user']
 }
