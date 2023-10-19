5d3bf982df8ee4e244a90f5047fc75e80a6ac511
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-10-19 14:58:10 +0300

    fix puppet-lint

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,12 @@
 # increase time limit
 
-# fix waiting time 
 exec {'fix--for-nginx':
     command => 'sed -i "s/15/4096/" /etc/default/nginx',
-    path => '/bin'
+    path    => '/bin'
 }
 
-# restarting nginx
 exec { 'restart':
-    command => 'service nginx restart',
-    path => '/usr/sbin:/sbin:/usr/bin:/bin',
+    command   => 'service nginx restart',
+    path      => '/usr/sbin:/sbin:/usr/bin:/bin',
     subscribe => Exec['fix--for-nginx']
 }
